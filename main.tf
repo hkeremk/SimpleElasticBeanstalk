16f1149b81617af3f7c98722acee59663a14dede
hkeremk <74671922+hkeremk@users.noreply.github.com>
2023-05-19 17:17:44 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,90 @@
+provider "aws" {
+
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_elastic_beanstalk_application" "myapp" {
+  name        = "test"
+  description = "Sample Test Application"
+}
+
+resource "aws_iam_instance_profile" "subject_profile" {
+  name = "test_role_new"
+  role = aws_iam_role.role.name
+}
+resource "aws_iam_role" "role" {
+  name = "test_role_new"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier",
+  ])
+
+  role       = aws_iam_role.role.name
+  policy_arn = each.value
+}
+
+resource "aws_elastic_beanstalk_environment" "myenv" {
+  name                = "hkeremkenvironment"
+  application         = aws_elastic_beanstalk_application.myapp.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.4.1 running Corretto 17"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.subject_profile.name
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "MatcherHTTPCode"
+    value     = "200"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.micro"
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = 1
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = 2
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "SystemType"
+    value     = "enhanced"
+  }
+}
